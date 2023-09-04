10336bbccdd3e298bfe2dca3f49d88dee6418c48
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2023-09-04 22:24:35 +0600

    create navbar and design done

--- src/Component/NavBar/NavBar.jsx
+++ src/Component/NavBar/NavBar.jsx
@@ -1,7 +1,25 @@
+import { Link } from "react-router-dom";
+
 const NavBar = () => {
   return (
-    <div>
-      <h1>Nav Bar </h1>
+    <div className="navbar bg-slate-900 text-slate-200 px-6">
+      <div className="navbar-start">
+        <Link className="font-bold">Local E-Store</Link>
+      </div>
+      <div className="navbar-end flex gap-x-8 font-semibold">
+        <Link className="hover:text-red-400" to="/">
+          Home
+        </Link>
+        <Link className="hover:text-red-400" to="/">
+          Shop
+        </Link>
+        <Link className="hover:text-red-400" to="/">
+          Order
+        </Link>
+        <Link to="/">
+          <button className="btn btn-outline btn-success btn-sm">login</button>
+        </Link>
+      </div>
     </div>
   );
 };
